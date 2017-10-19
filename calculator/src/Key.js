dc6e72013f69574952a2ebf4658acba31b56a07b
Kyle Aardal <aardal.kyle@gmail.com>
2017-10-19 11:45:41 -0700

    added reducer

--- calculator/src/Key.js
+++ calculator/src/Key.js
@@ -4,10 +4,16 @@ import RaisedButton from "material-ui/RaisedButton";
 class Key extends Component {
   constructor(props) {
     super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    return;
   }
 
   render() {
-    return <RaisedButton label={this.props.label} />;
+    return <RaisedButton label={this.props.label} onClick={this.handleClick} />;
   }
 }
 
---
+++ calculator/src/reducer/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import keyReducer from "./key-reducer.js";
+
+export default combineReducers({ keyReducer });
